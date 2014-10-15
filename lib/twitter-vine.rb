d627fb47c1a0a913a3d05954e1f8904c66616554
darren <darren.hicks@gmail.com>
2014-10-15 00:42:18 -0600

    Fixing to work with latest vine.co pages.  Bumping to 0.1.8

--- lib/twitter-vine.rb
+++ lib/twitter-vine.rb
@@ -14,11 +14,11 @@ module TwitterVine
         vine_id: vine_url.match(/.*\/(.*)/)[1],
         vine_url: vine_url,
         vine_thumbnail: doc.xpath("//meta[@property='og:image']").first[:content],
-        vine_author_thumbnail: doc.css(".avatar-container img").first[:src],
-        vine_author: doc.css("p.username").text,
-        vine_description: doc.css("p.description").text.gsub(/\s+/," ").strip,
-        vine_src: doc.css("video source").first[:src],
-        vine_type: doc.css("video source").first[:type]
+        vine_author_thumbnail: doc.css("article.post a img").first["src"],
+        vine_author: doc.css("article.post a span[itemprop=author]").text,
+        vine_description: doc.xpath("//meta[@property='og:description']").first[:content],
+        vine_src: doc.xpath("//meta[@property='twitter:player:stream']").first[:content],
+        vine_type: doc.xpath("//meta[@property='twitter:player:stream:content_type']").first[:content].split(";").first
     }
   end
 end
