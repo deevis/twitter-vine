e2efe62ae5b4201a6d13440667fabfbec21c57ac
Garrett Simpson <elgaricimo@gmail.com>
2015-06-11 15:55:51 -0700

    Fix vine_author key

--- lib/twitter-vine.rb
+++ lib/twitter-vine.rb
@@ -20,7 +20,7 @@ module TwitterVine
         vine_url: vine_url,
         vine_thumbnail: doc.xpath("//meta[@property='og:image']").first[:content],
         vine_author_thumbnail: doc.css("article.post a img").first["src"],
-        vine_author: doc.css("article.post a span[itemprop=author]").text,
+        vine_author: doc.xpath("//meta[@property='og:description']").first[:content].gsub("Vine by ", ""),
         vine_description: doc.xpath("//meta[@property='og:description']").first[:content],
         vine_src: doc.xpath("//meta[@property='twitter:player:stream']").first[:content],
         vine_type: doc.xpath("//meta[@property='twitter:player:stream:content_type']").first[:content].split(";").first
