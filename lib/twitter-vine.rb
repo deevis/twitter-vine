51a954800e2b4bccbee0bf604a6eff1d939d0e77
Garrett Simpson <elgaricimo@gmail.com>
2015-06-21 02:36:31 -0700

    Scrape out text of vine and hashtags of vine in parse method

--- lib/twitter-vine.rb
+++ lib/twitter-vine.rb
@@ -15,9 +15,11 @@ module TwitterVine
     rescue OpenURI::HTTPError
       return nil
     end
-    {
+    hashtag_regex = /#([A-Za-z0-9]+)/
+    parsed_vine = {
         vine_id: vine_url.match(/.*\/(.*)/)[1],
         vine_url: vine_url,
+        vine_text: doc.xpath("//div")[0].children[1].children[1].children[3].children[10].children[0].text,
         vine_thumbnail: doc.xpath("//meta[@property='og:image']").first[:content],
         vine_author_thumbnail: doc.css("article.post a img").first["src"],
         vine_author_profile_url: "https://vine.co" + doc.xpath("//a").last.attributes["href"].value,
@@ -26,5 +28,8 @@ module TwitterVine
         vine_src: doc.xpath("//meta[@property='twitter:player:stream']").first[:content],
         vine_type: doc.xpath("//meta[@property='twitter:player:stream:content_type']").first[:content].split(";").first
     }
+    parsed_vine[:vine_hashtags] = parsed_vine[:vine_text].scan(hashtag_regex).flatten
+
+    parsed_vine
   end
 end
